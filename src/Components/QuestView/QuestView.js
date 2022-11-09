feed40c513d983a2be225b396b7d6433db06b889
Scott Gaines <scottdgaines@gmail.com>
2022-11-09 08:29:31 -0700

    Refactor optionalLink conditional logic so it doesn't display with an error

--- src/Components/QuestView/QuestView.js
+++ src/Components/QuestView/QuestView.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom'
 import './QuestView.css'
 
 const QuestView = ({ currentQuest,  markCompleted, getData, completed, error }) => {
-    const optionalLink = !completed && currentQuest.link ? <p>Not sure where to start? Check out <a href={`${currentQuest.link}`} target="_blank">this link</a></p> : null
+    const optionalLink = !completed && currentQuest.link && !error ? <p>Not sure where to start? Check out <a href={`${currentQuest.link}`} target="_blank">this link</a></p> : null
     const header = !completed ? <div><h2>{currentQuest.activity}</h2>{optionalLink}</div> : <h2>Well done, Traveler!</h2>
     const dynamicButton = !completed ? 
         <Link to="quest-complete">
