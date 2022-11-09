9160552273978b867bda9b2dbca0024994909d28
Scott Gaines <scottdgaines@gmail.com>
2022-11-09 08:51:11 -0700

    Adjust error handling in QuestView, add error handling to main page

--- src/Components/QuestView/QuestView.js
+++ src/Components/QuestView/QuestView.js
@@ -5,18 +5,14 @@ import './QuestView.css'
 const QuestView = ({ currentQuest,  markCompleted, getData, completed, error }) => {
     const optionalLink = !completed && currentQuest.link && !error ? <p>Not sure where to start? Check out <a href={`${currentQuest.link}`} target="_blank">this link</a></p> : null
     const header = !completed ? <div><h2>{currentQuest.activity}</h2>{optionalLink}</div> : <h2>Well done, Traveler!</h2>
-    const dynamicButton = !completed ? 
+    const dynamicButton = !completed && !error ? 
         <Link to="quest-complete">
             <button onClick={() => markCompleted(currentQuest)}>Mark as Complete</button>
         </Link> :
         <Link to="view-all-completed">
             <button>View Completed</button>
         </Link>
-    const errorMessage = error ? <h2>The dark lord is afoot, and there was an error! Please persist, and try again!</h2> : null
-
-    useEffect(() => {
-        getData()
-    }, [])
+    const errorMessage = error ? <p>Another quest cannot be granted at this time. Confound that dark lord!</p> : null
 
     return (
         <section>
