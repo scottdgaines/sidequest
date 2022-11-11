1e0d234d825f021589495177387e7dae6e83a157
Scott Gaines <scottdgaines@gmail.com>
2022-11-10 19:34:31 -0700

    Refactor large ternaries to if/else statement

--- src/Components/QuestView/QuestView.js
+++ src/Components/QuestView/QuestView.js
@@ -4,27 +4,50 @@ import PropTypes from 'prop-types'
 import './QuestView.css'
 
 const QuestView = ({ currentQuest,  markCompleted, getData, completed, error }) => {
-    const optionalLink = !completed && currentQuest.link && !error ? 
-        <p className="quest-text">Not sure where to start? Check out <a href={`${currentQuest.link}`} target="_blank">this link</a>
-        </p> : null
-    const header = !completed ? 
-        <div className="quest-name-container">
-            <h2 className="quest-header">{currentQuest.activity}</h2>
-            {optionalLink}
-        </div> : 
-        <div><h2 className="quest-header">Well done, Traveler!</h2><p className="quest-text">Your quest is complete. <br /> What would you like to do now?</p></div>
-    const dynamicButton = !completed && !error ? 
-        <Link to="quest-complete">
-            <button 
-                onClick={() => {
-                    markCompleted(currentQuest)
-                    getData()}}
-                className="quest-button">Mark as Complete</button>
-        </Link> :
-        <Link to="view-all-completed">
-            <button className="quest-button">View Completed</button>
-        </Link>
+    let optionalLink;
+    let header;
+    let dynamicButton;
     const errorMessage = error ? <p className="quest-text">Another quest cannot be granted at this time. Confound that Dark Lord!</p> : null
+
+    //Defining optionalLink
+    if (!completed && currentQuest.link) {
+        optionalLink = <p className="quest-text">Not sure where to start? Check out <a href={`${currentQuest.link}`} target="_blank">this link</a></p>
+    } 
+
+    //Defining header
+    if (!completed) {
+        header = 
+            <div className="quest-name-container">
+                <h2 className="quest-header">{currentQuest.activity}</h2>
+                {optionalLink}
+            </div>
+    } else {
+        header = 
+            <div>
+                <h2 className="quest-header">Well done, Traveler!</h2>
+                <p className="quest-text">Your quest is complete. 
+                <br /> What would you like to do now?</p>
+            </div>
+    }
+    
+    //Defining dynamicButton
+    if (!completed && !error) {
+        dynamicButton = 
+            <Link to="quest-complete">
+                <button 
+                    onClick={() => {
+                        markCompleted(currentQuest)
+                        getData()}}
+                    className="quest-button">
+                    Mark as Complete
+                </button>
+            </Link>
+    } else {
+        dynamicButton = 
+            <Link to="view-all-completed">
+                <button className="quest-button">View Completed</button>
+            </Link>
+    }
 
     return (
         <section className="scroll new-quest">
