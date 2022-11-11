154faa698596ee1da6cc0c2f74772965fb1a3642
Scott Gaines <scottdgaines@gmail.com>
2022-11-11 08:53:05 -0700

    Add theme object file, and theme setting form

--- src/App.js
+++ src/App.js
@@ -7,15 +7,18 @@ import cleanData from './utilities';
 import './App.css';
 import wizard from './assets/wizard.png';
 import titleBanner from './assets/title-banner.png';
+import themes from './themes.js';
 
 const App = () => {
   const [currentQuest, setCurrentQuest] = useState({});
   const [completedQuests, setCompletedQuests] = useState([]);
+  const [theme, setTheme] = useState({})
   const [error, setError] = useState('');
 
   const errorMessage = error ? <p className="welcome-message">The Dark Lord is afoot! For your safety, I cannot grant quests at this time. But fear not! You can try again later.</p> : null;
   const welcomeMessage = !error ? <p className="welcome-message">Welcome, Traveler! <br />I have a quest for you! <br />What would you like to do?</p> : errorMessage;
   const conditionalButton = !error ?   <Link to="/new-quest"><button>View Your Quest</button></Link> : null;
+  const { forrest, castle, meadow } = themes;
 
   const getData = async () => {
     try {    
@@ -79,6 +82,15 @@ const App = () => {
         />
         <Route component={NotFound} />
       </Switch>
+      <form className="theme-setting-container">
+        <p className="quest-text">choose your theme</p>
+        <input type="radio" name="theme" value="haunted-forrest" />
+        <label for="haunted-forrest" className="quest-text theme-button">Haunted Forrest</label>
+        <input type="radio" name="theme" value="castle" />
+        <label for="castle" className="quest-text theme-button">Castle</label>
+        <input type="radio" name="theme" value="meadow" checked="true"/>
+        <label for="meadow" className="quest-text theme-button">Meadow</label>
+      </form>
     </main>
   );
 };
---
+++ src/themes.js
@@ -0,0 +1,20 @@
+const themes = {
+   forrest: {
+        background: 'forrest-background',
+        character: './assets/witch.png',
+        greeting: 'Wandering One'
+
+    },
+  castle: {
+        background: 'castle-background',
+        character: './assets/king.png',
+        greeting: 'Noble One'
+    },
+   meadow: {
+        background: 'meadow-background',
+        character: './assets/wizard.png',
+        greeting: 'Traveler'
+    }
+}
+
+export default themes
